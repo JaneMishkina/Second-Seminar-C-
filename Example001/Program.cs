26a69494064c79e00cc467d0e027f1e04fbe6c0a
Evgenia <janemishkina@yandex.ru>
2023-01-20 16:26:20 +0300

    Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа

---
+++ Example001/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int GetRandomValue()
+{
+    return new Random().Next(100, 1000);
+}
+
+int FindThirdNumber(int number)
+{
+    return (number / 10) %10;
+}
+
+int number = GetRandomValue();
+int result = FindThirdNumber(number);
+Console.WriteLine(number);
+Console.WriteLine(result);
