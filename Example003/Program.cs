af8fdfdebfeb2e80ac7d187083ff15ece0d96ec1
Evgenia <janemishkina@yandex.ru>
2023-01-20 17:44:21 +0300

    Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным(короткая)

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -7,31 +7,11 @@
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number == 1)
+if (number >= 1 && number <= 5)
 {
     Console.WriteLine($"нет");
 }
-else if (number == 2)
-{
-    Console.WriteLine($"нет");
-}
-else if (number == 3)
-{
-    Console.WriteLine($"нет");
-}
-else if (number == 4)
-{
-    Console.WriteLine($"нет");
-}
-else if (number == 5)
-{
-    Console.WriteLine($"нет");
-}
-else if (number == 6)
-{
-    Console.WriteLine($"да");
-}
-else if (number == 7)
+else if (number == 6 || number ==7)
 {
     Console.WriteLine($"да");
 }
